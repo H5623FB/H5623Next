f347c7794427604e16ea2fa51c01232269c3fd0c
ILEC H5623 <h5623fb@gmail.com>
2018-11-02 18:22:59 +0000

    1

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+const About = () => (
+    <Layout>  
+    <div>
+          <h1>This is About page</h1>
+    </div>
+    </Layout>
+    );
+    
+    export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+const Index = () => (
+<Layout>
+    <div> 
+        <h1>Hello Next.Js</h1>
+    </div>
+</Layout>
+);
+
+export default Index;
